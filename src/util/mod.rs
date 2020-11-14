462b9c267cd6ba7eddef9f888a1758cb4e93aff8
Claus Martinsen <clauswrm@gmail.com>
2020-11-14 23:37:43 +0100

    Add prettier board UTF-8 formatting

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,7 +1,7 @@
 use chess::Board;
 use std::fmt::Write;
 
-pub fn print_board(board: &Board) {
+pub fn print_board_ascii(board: &Board) {
     let fen_string = format!("{}", board);
     let mut fen_itr = fen_string.split_whitespace();
 
@@ -37,6 +37,63 @@ pub fn print_board(board: &Board) {
     write!(
         &mut res,
         "|\n   +------------------------+\n     a  b  c  d  e  f  g  h\n"
+    )
+    .unwrap();
+
+    println!("{}", res);
+}
+
+pub fn print_board(board: &Board) {
+    let fen_string = format!("{}", board);
+    let mut fen_itr = fen_string.split_whitespace();
+
+    let pieces = fen_itr.next().unwrap();
+    let player = fen_itr.next().unwrap();
+    let _casteling_rights = fen_itr.next().unwrap();
+    let _en_passant_rights = fen_itr.next().unwrap();
+    let _half_move_clock = fen_itr.next().unwrap();
+    let full_move_number = fen_itr.next().unwrap();
+
+    let mut res = String::new();
+
+    write!(
+        &mut res,
+        "Turn {} - {} to move.\n\n",
+        full_move_number, player
+    )
+    .unwrap();
+
+    write!(&mut res, "   ┌────────────────────────┐\n 8 │").unwrap();
+    let mut rank = 7;
+    for ch in pieces.chars() {
+        if ch == '/' {
+            write!(&mut res, "│\n {} │", rank).unwrap();
+            rank -= 1;
+        } else if ch.is_digit(10) {
+            let num = ch.to_digit(10).unwrap();
+            write!(&mut res, "{}", " . ".repeat(num as usize)).unwrap();
+        } else {
+            let piece_ch = match ch {
+                'K' => "♔",
+                'Q' => "♕",
+                'R' => "♖",
+                'B' => "♗",
+                'N' => "♘",
+                'P' => "♙",
+                'k' => "♚",
+                'q' => "♛",
+                'r' => "♜",
+                'b' => "♝",
+                'n' => "♞",
+                'p' => "♟",
+                _ => panic!("Illegal char found!"),
+            };
+            write!(&mut res, " {} ", piece_ch).unwrap();
+        }
+    }
+    write!(
+        &mut res,
+        "│\n   └────────────────────────┘\n     a  b  c  d  e  f  g  h\n"
     )
     .unwrap();
 
