13f08bade9ca95c0ed49c286db1ec46414bd3bbe
Claus Martinsen <clauswrm@gmail.com>
2022-10-17 17:13:42 +0200

    Handle more errors

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,14 @@ use tokio_stream::StreamExt;
 
 #[tokio::main]
 async fn main() {
-    const DEPTH: u8 = 3;
+    const DEPTH: u8 = 5;
     let bot_player = Bot { depth: DEPTH };
 
     let client = Licheszter::new(String::from("lip_PV83SUBeitbrybhXD57C"));
-    let mut stream = client.stream_events().await.unwrap();
+    let mut stream = client
+        .stream_events()
+        .await
+        .expect("Error while streaming events.");
     let mut opponent_name = String::from("");
     let mut bot_color = Color::Black;
 
@@ -29,15 +32,20 @@ async fn main() {
                     println!(
                         "[{}] Challenge recieved. Time control: {}.",
                         challenge.id,
-                        challenge.time_control.show.unwrap()
+                        challenge.time_control.show.unwrap_or(String::from("n/a"))
                     );
-                    client.challenge_accept(&challenge.id).await.unwrap();
+                    client
+                        .challenge_accept(&challenge.id)
+                        .await
+                        .expect("Error when accepting challenge.");
                 }
             }
             Event::GameStart { game: game_id } => {
                 let mut game = Game::new();
-                let mut stream = client.stream_game_state(&game_id.id).await.unwrap();
-                let mut last_move: &str;
+                let mut stream = client
+                    .stream_game_state(&game_id.id)
+                    .await
+                    .expect("Error while streaming game state.");
                 while let Ok(Some(state)) = stream.try_next().await {
                     match state {
                         BoardState::GameFull(game_full) => {
@@ -58,55 +66,69 @@ async fn main() {
                                 client
                                     .make_move(&game_id.id, &uci_move, false)
                                     .await
-                                    .unwrap();
+                                    .expect("Error when making move.");
                             }
                         }
                         BoardState::GameState(game_state) => {
-                            println!("[{}] {}", game_id.id, game_state.moves);
                             if game_state.status == "started" {
-                                last_move = game_state.moves.rsplitn(2, " ").next().unwrap();
+                                let last_move = game_state
+                                    .moves
+                                    .rsplitn(2, " ")
+                                    .next()
+                                    .expect("Move string should contain a substring when splitting by space.");
+                                println!("[{}] Move made: {}", game_id.id, last_move);
                                 if let Ok(chess_move) = ChessMove::from_str(last_move) {
-                                    game.make_move(chess_move);
-                                    if game.side_to_move() == bot_color {
-                                        let board = game.current_position();
-                                        let chosen_move = bot_player.choose_move(&board);
-                                        let uci_move = format!("{}", chosen_move);
-                                        client
-                                            .make_move(&game_id.id, &uci_move, false)
-                                            .await
-                                            .unwrap();
+                                    let move_result = game.make_move(chess_move);
+                                    if move_result {
+                                        if game.side_to_move() == bot_color {
+                                            let board = game.current_position();
+                                            let chosen_move = bot_player.choose_move(&board);
+                                            let uci_move = format!("{}", chosen_move);
+                                            client
+                                                .make_move(&game_id.id, &uci_move, false)
+                                                .await
+                                                .expect("Error when making move.");
+                                        }
+                                    } else {
+                                        println!(
+                                            "[{}] Move could not be made: '{}'.",
+                                            game_id.id, last_move
+                                        );
                                     }
                                 } else {
                                     println!(
                                         "[{}] Illegal move recieved: '{}'.",
                                         game_id.id, last_move
-                                    )
+                                    );
                                 }
                             } else {
                                 println!(
                                     "[{}] Game ended with status {}.",
                                     game_id.id, game_state.status
-                                )
+                                );
                             }
                         }
                         game_state => {
                             println!(
                                 "[{}] Other game state recieved: {:?}",
                                 game_id.id, game_state
-                            )
+                            );
                         }
                     }
                 }
             }
             Event::GameFinish { game: game_id } => {
-                println!("[{}] Finished.", game_id.id)
+                println!("[{}] Finished.", game_id.id);
+                break;
             }
             Event::ChallengeCanceled { challenge } => {
-                println!("[{}] Cancelled.", challenge.id)
+                println!("[{}] Cancelled.", challenge.id);
+                break;
             }
             event => {
-                println!("Other event recieved: {:?}", event)
+                println!("Other event recieved: {:?}", event);
             }
         }
     }
+    println!("Shutting down...");
 }
